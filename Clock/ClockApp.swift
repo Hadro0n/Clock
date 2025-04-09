53e327b57de12918ede743b3257fe1b61d1930f6
Hadro0n <v.w.yakunin@gmail.com>
2025-04-09 13:11:58 +0300

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Владислав Якунин on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
